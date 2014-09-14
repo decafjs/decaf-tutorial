3c81e7b6a0f055ce8be48a61c2e8785a06673e9c
Michael Schwartz <mykes@sportstwo.com>
2014-09-14 14:21:21 -0700

    phase 3 in progress

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
 
 var Application = require('decaf-jolt').Application,
     StaticFile = require('decaf-jolt').StaticFile,
+    StaticServer = require('decaf-jolt').StaticServer,
     SjsFile = require('decaf-jolt').SjsFile,
     SjsServer = require('decaf-jolt').SjsServer,
     app = new Application();
@@ -13,8 +14,26 @@ var Application = require('decaf-jolt').Application,
 // serve static/favicon.ico when URL is /favicon.ico
 app.verb('favicon.ico', StaticFile('static/favicon.ico'));
 
+// serve bower_components from bower_components directory
+app.verb('bower_components', StaticServer('bower_components'));
+
+// serve css stylesheets from css directory when the URL starts with /css/
+app.verb('css', StaticServer('css'));
+
+// serve js files from js directory when the URL starts with /js/
+app.verb('js', StaticServer('js'));
+
+// serve image files from images directory when the URL starts with /images/
+app.verb('images', StaticServer('images'));
+
 // serve / URL via Home controller
 app.verb('/', SjsFile('controllers/Home.sjs'));
+
+// serve /Page URL via Page controller
+app.verb('Page', SjsFile('controllers/Page.sjs'));
+
+// Web services/API at /API/whatever.sjs the api subdirectory
+app.verb('API', SjsServer('api'));
 
 // '127.0.0.1' is localhost only
 // '0.0.0.0' to listen on all interfaces
